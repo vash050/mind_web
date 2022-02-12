9865a0d7eeece89a13913334bcc685880c3edf04
Vasiliy <vash050@rambler.ru>
2022-02-12 16:18:57 +0300

    urls setting

---
+++ customerapp/urls.py
@@ -0,0 +1,15 @@
+
+from django.contrib import admin
+from django.urls import path
+
+import customerapp.views as customerapp
+
+app_name = 'customerapp'
+
+urlpatterns = [
+
+]
+
+urlpatterns = [
+
+]
---
+++ mainapp/urls.py
@@ -0,0 +1,11 @@
+
+from django.contrib import admin
+from django.urls import path
+
+import mainapp.views as mainapp
+
+app_name = 'mainapp'
+
+urlpatterns = [
+
+]
