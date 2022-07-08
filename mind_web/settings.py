2ddbf4d141441c65006f09130c3d65fd4c05c844
Vasiliy <vash050@rambler.ru>
2022-07-08 14:25:56 +0300

    security

--- mind_web/settings.py
+++ mind_web/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
+from tmp import SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-iu5d5ow+ly1*1s%x5#f(&%raz5e^+v@=lg^yq(x^)9ej04mtmx'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
